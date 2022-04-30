8f7417ee861f35e87ac25cd45e12e42e1d33842f
daniil <trostin1998@bk.ru>
2022-05-01 00:04:34 +0300

    HW7 done

--- src/ru/test/Main.java
+++ src/ru/test/Main.java
@@ -1,8 +1,29 @@
 package ru.test;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-
+//        Task 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+//        Task 2
+        String needName = fullName.toUpperCase(Locale.ROOT);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + needName);
+//        Task 3
+        String fulName = "Иванов Семён Семёнович";
+        fulName.replace("ё", "е");
+        String[] words = fulName.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].contains("ё")) {
+                words[i].replace('ё', 'е');
+            }
+            System.out.println(words[i]);
+        }
+        System.out.println("Данные ФИО сотрудника - " + fulName);
     }
 }
